9fa9ba5160688ef61e828d5581c35798e0701d7a
Sebastian Vargas <vargaspizango1@gmail.com>
2021-11-30 22:03:02 -0500

    chore: add components

--- src/app/modules/home/home.module.ts
+++ src/app/modules/home/home.module.ts
@@ -1,15 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './pages/home/home.component';
-
-
+import { SharedModule } from 'src/app/shared/shared.module';
 
 @NgModule({
-  declarations: [
-    HomeComponent
-  ],
-  imports: [
-    CommonModule
-  ]
+  declarations: [HomeComponent],
+  imports: [CommonModule, SharedModule],
+  exports: [HomeComponent],
 })
-export class HomeModule { }
+export class HomeModule {}
