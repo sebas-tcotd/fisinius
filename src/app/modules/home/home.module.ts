751603bae3dec084d990c3babb50aeffb6965df2
Sebastian Vargas <vargaspizango1@gmail.com>
2021-11-30 22:26:18 -0500

    feat: add first lazy routing

--- src/app/modules/home/home.module.ts
+++ src/app/modules/home/home.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './pages/home/home.component';
 import { SharedModule } from 'src/app/shared/shared.module';
+import { HomeRoutingModule } from './home-routing.module';
 
 @NgModule({
   declarations: [HomeComponent],
-  imports: [CommonModule, SharedModule],
+  imports: [CommonModule, HomeRoutingModule, SharedModule],
   exports: [HomeComponent],
 })
 export class HomeModule {}
