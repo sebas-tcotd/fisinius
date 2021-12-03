a5008586cf77438653f20a472098be25bf8a929f
Sebastian Vargas <vargaspizango1@gmail.com>
2021-12-02 21:23:03 -0500

    feat: add footer

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -4,10 +4,16 @@ import { HeaderComponent } from './components/header/header.component';
 import { ImgNewsPipe } from './pipes/img-news.pipe';
 import { RouterModule } from '@angular/router';
 import { HeadlineComponent } from './components/headline/headline.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
-  declarations: [HeaderComponent, ImgNewsPipe, HeadlineComponent],
+  declarations: [
+    HeaderComponent,
+    ImgNewsPipe,
+    HeadlineComponent,
+    FooterComponent,
+  ],
   imports: [CommonModule, RouterModule],
-  exports: [HeaderComponent, ImgNewsPipe, HeadlineComponent],
+  exports: [HeaderComponent, ImgNewsPipe, HeadlineComponent, FooterComponent],
 })
 export class SharedModule {}
