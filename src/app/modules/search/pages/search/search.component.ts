412bd980d5d2ab8b350c07770340dbe662717861
Sebastian Vargas <vargaspizango1@gmail.com>
2021-12-06 00:17:06 -0500

    feat: add second iteration of search

--- src/app/modules/search/pages/search/search.component.ts
+++ src/app/modules/search/pages/search/search.component.ts
@@ -1,4 +1,10 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
+import {
+  Component,
+  OnChanges,
+  OnDestroy,
+  OnInit,
+  SimpleChanges,
+} from '@angular/core';
 import { Subscription } from 'rxjs';
 import { NewsService } from 'src/app/core/services/news.service';
 import { NewsElement } from '../../../../core/interfaces/news.interfaces';
@@ -9,21 +15,32 @@ import { NewsElement } from '../../../../core/interfaces/news.interfaces';
   styleUrls: ['./search.component.scss'],
 })
 export class SearchComponent implements OnInit, OnDestroy {
+  isPageLoading: boolean = true;
   headlines: NewsElement[] = [];
   subscription!: Subscription;
 
   constructor(private newsService: NewsService) {}
 
   ngOnInit(): void {
-    this.subscription = this.newsService.searchKeyword$.subscribe(
-      (res) => {
-        console.log(res);
-        this.headlines = res.news;
-      },
-      (err) => {
-        console.log(err);
-      }
-    );
+    this.waitForNews();
+  }
+
+  waitForNews() {
+    this.isPageLoading = true;
+    this.getResultsFromSearch();
+
+    setTimeout(() => {
+      this.isPageLoading = false;
+    }, 4000);
+  }
+
+  getResultsFromSearch() {
+    this.subscription = this.newsService.searchKeyword$.subscribe((res) => {
+      console.log(res);
+
+      this.isPageLoading = false;
+      this.headlines = res.news;
+    });
   }
 
   ngOnDestroy() {
